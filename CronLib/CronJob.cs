bfe123d665f5641a978bf513f5bf691904f0fd90
claus <claus.z@protonmail.com>
2018-06-21 21:36:07 +1000

    Move the CronJob class into the library

---
+++ CronLib/CronJob.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+using Quartz;
+using Quartz.Impl;
+using Quartz.Logging;
+
+namespace CronLib
+{
+    public class CronJob<TheJob> where TheJob : IJob
+    {
+        public static async Task Run()
+        {
+            try
+            {
+                NameValueCollection props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+
+                StdSchedulerFactory factory = new StdSchedulerFactory(props);
+                IScheduler scheduler = await factory.GetScheduler();
+
+                // and start it off
+                await scheduler.Start();
+
+                // define the job and tie it to our HelloJob class
+                IJobDetail job = JobBuilder.Create<TheJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+                // Trigger the job to run now, and then repeat every 10 seconds
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger3", "group1")
+                    .WithCronSchedule("0 0/1 8-23 * * ?")
+                    .ForJob("job1", "group1")
+                    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                await scheduler.ScheduleJob(job, trigger);
+
+                // some sleep to show what's happening
+                await Task.Delay(TimeSpan.FromSeconds(300));
+
+                // and last shut down the scheduler when you are ready to close your program
+                await scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                await Console.Error.WriteLineAsync(se.ToString());
+            }
+        }
+
+        // simple log provider to get something to the console
+        private class ConsoleLogProvider : ILogProvider
+        {
+            public Logger GetLogger(string name)
+            {
+                return (level, func, exception, parameters) =>
+                {
+                    if (level >= LogLevel.Info && func != null)
+                    {
+                        Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] [" + level + "] " + func(), parameters);
+                    }
+                    return true;
+                };
+            }
+
+            public IDisposable OpenNestedContext(string message)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IDisposable OpenMappedContext(string key, string value)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+}
